353c44d782edd73b0662caaeffb4e69d2822dc5f
Nusab Taha <85403795+Nusab19@users.noreply.github.com>
2025-06-10 19:18:32 +0600

    Update changeColor.js

--- wikipedia/changeColor.js
+++ wikipedia/changeColor.js
@@ -4,10 +4,13 @@
 // @version      2025-06-10
 // @description  try to take over the world!
 // @author       You
-// @match        https://en.m.wikipedia.org/wiki/Berry_paradox
+// @match        https://*.wikipedia.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikipedia.org
 // @grant        none
 // ==/UserScript==
+
+
+// NOTE: Code is unfinished. More changes are needed to make it perfect.
 
 (function() {
     'use strict';
@@ -16,10 +19,19 @@
         "--background-color-base": "#f4e9b1",
         "--background-color-interactive-subtle": "#f0e08f",
         "--background-color-base-fixed": "#fff1a9",
-        "--background-color-interactive": "#ffec8d"
+        "--background-color-interactive": "#ffec8d",
+        "--color-base": "#282828",
+        "--color-base--hover": "#232f3c",
+        "--color-emphasized": "#113c66",
+        "--color-subtle": "#a2a9b1",
+        "--color-progressive": "#003db6",
     };
 
     for (const [prop, value] of Object.entries(styles)) {
         document.documentElement.style.setProperty(prop, value);
     }
+
+    // Logo color always black
+    document.querySelector(".mw-logo-container").className = "mw-logo-container"
+
 })();
