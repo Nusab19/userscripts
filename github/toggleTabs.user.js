1b65e6cbca32f47168c91d2849b4cea0b19e1bf6
Nusab Taha <85403795+Nusab19@users.noreply.github.com>
2025-09-02 22:23:17 +0600

    Create toggleTabs.user.js

---
+++ github/toggleTabs.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Github - Toggle Between Tabs with Alt+` (Edit, Preview, Code, Blame)
+// @namespace    http://tampermonkey.net/
+// @version      2025-09-02
+// @description  Toggles between GitHub tabs (Edit, Preview, Code, Blame) using Alt+`
+// @author       Nusab Taha
+// @match        https://github.com/*/edit/*
+// @match        https://github.com/*/blob/*
+// @match        https://github.com/*/blame/*
+// @match        https://github.com/*/new/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.addEventListener('keydown', function(event) {
+        // Check if Alt + ` (backtick) is pressed
+        if (event.altKey && event.code === 'Backquote') {
+            event.preventDefault(); // Prevent default browser behavior
+            document.querySelector(`[aria-current="false"]`).click()
+        }
+    });
+})();
