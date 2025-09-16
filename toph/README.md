b1c7c1e499bbd6a3e0c1e7d6c6ec4ed33ce5318c
Nusab Taha <nusabtaha33@gmail.com>
2025-09-16 21:27:05 +0600

    toph: own tweaks

---
+++ toph/README.md
@@ -0,0 +1,5 @@
+## Note
+
+Toph already [implemented keyboard shortcuts](https://community.toph.co/t/feature-keyboard-shortcuts).
+
+But, I just prefer some little own tweaks. So this is for me. You can visit the given discussion link and find the keyboard shortcuts details from there.
--- toph/keyboardShortcuts.user.js
+++ toph/keyboardShortcuts.user.js
@@ -9,72 +9,95 @@
 // @grant        none
 // ==/UserScript==
 
-
 const shortcuts = new Map();
 
 function shortcut(keys, callback) {
-    shortcuts.set(keys.toLowerCase(), callback);
+  shortcuts.set(keys.toLowerCase(), callback);
 }
 
 function getKeyCombo(e) {
-    const parts = [];
-    if (e.ctrlKey) parts.push('ctrl');
-    if (e.altKey) parts.push('alt');
-    if (e.shiftKey) parts.push('shift');
-    if (e.metaKey) parts.push('meta');
-    parts.push(e.key.toLowerCase());
-    return parts.join('+');
+  const parts = [];
+  if (e.ctrlKey) parts.push("ctrl");
+  if (e.altKey) parts.push("alt");
+  if (e.shiftKey) parts.push("shift");
+  if (e.metaKey) parts.push("meta");
+  parts.push(e.key.toLowerCase());
+  return parts.join("+");
 }
 
-document.addEventListener('keydown', e => {
+function handleKeydown(e) {
+  const combo = getKeyCombo(e);
+  const callback = shortcuts.get(combo);
+  if (callback) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    callback(e);
+    return false;
+  }
+}
+
+document.addEventListener("keydown", handleKeydown, true);
+document.addEventListener(
+  "keyup",
+  function (e) {
     const combo = getKeyCombo(e);
-    const callback = shortcuts.get(combo);
-    if (callback) {
-        e.preventDefault();
-        callback(e);
+    if (shortcuts.has(combo)) {
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      return false;
     }
-}, true);
+  },
+  true,
+);
 
-(function() {
-    'use strict';
-    window.shortcut = shortcut;
+(function () {
+  "use strict";
+  window.shortcut = shortcut;
+  let _meh = 0;
 
-    const isEditorOpen = () => document.getElementsByClassName("codepanel")[0]?.className.includes("-open")
-    const closeEditor = () => document.querySelector(".btn-close")?.click();
-    const toggleEditor = () => {
-        const openBtn = document.querySelector('[data-goatcounter-title="Opened IDE"]');
-        if (isEditorOpen()) {
-            closeEditor()
-        } else if (openBtn) {
-            openBtn.click()
-        }
-    };
-
-    const testRunCode = () => {
-        if (isEditorOpen()) {
-            document.getElementsByClassName("btn-test-samples")[0]?.click()
-        } else {
-            console.log("Open the Editor first to test run the code")
-        }
-    };
-
-    const submitCode = () => {
-        if (isEditorOpen()) {
-            document.getElementsByClassName("btn-submit")[0]?.click()
-        } else {
-            console.log("Open the Editor first to submit the code")
-        }
-    };
-
-    const toggleLanguageSelector = () => {
-        document.getElementsByClassName("form__field dropdown__toggle")[1]?.click()
-        // TODO: Add functionality to go through the languages using the up/down arrow keys
+  const isEditorOpen = () =>
+    document
+      .getElementsByClassName("codepanel")[0]
+      ?.className.includes("-open");
+  const closeEditor = () => document.querySelector(".btn-close")?.click();
+  const toggleEditor = () => {
+    const openBtn = document.querySelector(
+      '[data-goatcounter-title="Opened IDE"]',
+    );
+    _meh += 1;
+    if (_meh % 2 == 0 && isEditorOpen()) {
+      closeEditor();
     }
+    // else if (openBtn) {
+    //   openBtn.click();
+    // }
+  };
 
-    shortcut("ctrl+e", toggleEditor);
-    shortcut("escape", closeEditor);
-    shortcut("ctrl+enter", testRunCode);
-    shortcut("ctrl+shift+enter", submitCode);
-    shortcut("ctrl+l", toggleLanguageSelector);
+  const testRunCode = () => {
+    if (isEditorOpen()) {
+      document.getElementsByClassName("btn-test-samples")[0]?.click();
+    } else {
+      console.log("Open the Editor first to test run the code");
+    }
+  };
 
+  const submitCode = () => {
+    if (isEditorOpen()) {
+      document.getElementsByClassName("btn-submit")[0]?.click();
+    } else {
+      console.log("Open the Editor first to submit the code");
+    }
+  };
+
+  const toggleLanguageSelector = () => {
+    document.getElementsByClassName("form__field dropdown__toggle")[1]?.click();
+  };
+
+  shortcut("ctrl+e", toggleEditor);
+  // shortcut("escape", closeEditor);
+  // shortcut("ctrl+enter", testRunCode);
+  // shortcut("ctrl+shift+enter", submitCode);
+  shortcut("ctrl+l", toggleLanguageSelector);
 })();
